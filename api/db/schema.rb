e224c3d39916cbf7bf74ed28e133b95040280abd
Kelson <kelsonbatista@gmail.com>
2023-11-04 09:36:46 -0300

    added posts scaffold

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_04_121023) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_04_122408) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "topic_id", null: false
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "topics"
 end
